d27a64e9b9a9c84610e6f0291dd0b1c0af493705
ccmsm2022b27 <118444039+ccmsm2022b27@users.noreply.github.com>
2022-11-17 09:13:37 -0300

    panela

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+let xbol=200;
+let ybol=200;
+let diametro=50;
+function draw() {
+  background(0);
+circle (xbol, ybol, diametro);
+xbol= xbol+1;
+}
